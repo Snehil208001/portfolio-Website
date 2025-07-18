1bfb200a75b57f6e7ed095864673c0acda28da20
Snehil <snehilkumar94@gmail.com>
2025-07-18 10:38:33 +0530

    update

--- Portfolio Website/App.tsx
+++ Portfolio Website/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import AboutSection from './components/AboutSection';
-import SkillsSection from './components/SkillsSection';
-import ProjectsSection from './components/ProjectsSection';
-import ContactSection from './components/ContactSection';
-import Footer from './components/Footer';
-import { YOUR_NAME, YOUR_HEADLINE, YOUR_BIO, YOUR_PROFILE_PICTURE_URL, PROJECTS_DATA, SKILLS_DATA, CONTACT_LINKS_DATA, YOUR_EMAIL, COPYRIGHT_TEXT, NAV_LINKS } from './constants';
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+import SkillsSection from "./components/SkillsSection";
+import ProjectsSection from "./components/ProjectsSection";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
+import {
+  YOUR_NAME,
+  YOUR_HEADLINE,
+  YOUR_BIO,
+  PROJECTS_DATA,
+  SKILLS_DATA,
+  CONTACT_LINKS_DATA,
+  YOUR_EMAIL,
+  COPYRIGHT_TEXT,
+  NAV_LINKS,
+} from "./constants";
+import Profile from "./src/assets/snehil-profile.jpg";
 
 const App: React.FC = () => {
   return (
@@ -14,17 +25,30 @@ const App: React.FC = () => {
       <Navbar navLinks={NAV_LINKS} siteTitle={YOUR_NAME} />
 
       <main className="flex-grow">
-        <HeroSection 
-          name={YOUR_NAME} 
-          headline={YOUR_HEADLINE} 
-          profileImageUrl={YOUR_PROFILE_PICTURE_URL}
+        <HeroSection
+          name={YOUR_NAME}
+          headline={YOUR_HEADLINE}
+          profileImageUrl={Profile}
           ctaLink="#projects"
           ctaText="View My Work"
         />
         <AboutSection id="about" bio={YOUR_BIO} title={`About ${YOUR_NAME}`} />
-        <SkillsSection id="skills" skills={SKILLS_DATA} title="My Skills & Proficiencies" />
-        <ProjectsSection id="projects" projects={PROJECTS_DATA} title="Featured Projects" />
-        <ContactSection id="contact" email={YOUR_EMAIL} contactLinks={CONTACT_LINKS_DATA} title="Get In Touch" />
+        <SkillsSection
+          id="skills"
+          skills={SKILLS_DATA}
+          title="My Skills & Proficiencies"
+        />
+        <ProjectsSection
+          id="projects"
+          projects={PROJECTS_DATA}
+          title="Featured Projects"
+        />
+        <ContactSection
+          id="contact"
+          email={YOUR_EMAIL}
+          contactLinks={CONTACT_LINKS_DATA}
+          title="Get In Touch"
+        />
       </main>
 
       <Footer copyrightText={COPYRIGHT_TEXT} />
